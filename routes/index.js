d49fc13886918a4629795aececc301fa65ea2a21
dpmanek <dpmanek@gmail.com>
2022-07-01 20:08:25 -0400

    change

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-
 const usersRoutes = require('./users');
-const userDataRoutes = require('./userData')
-const publicRoutes = require('./public')
+const userDataRoutes = require('./userData');
+const publicRoutes = require('./public');
 
 const constructorMethod = (app) => {
-	
 	app.use('/users', usersRoutes); // Login/Signup (API)Routes
 	app.use('/user/data', userDataRoutes); // verify user and get specific data  Uses Middle Ware to Verify
 	app.use('/public', publicRoutes); // No Middle Ware route should display all public Data
-
+	app.get('/', (req, res) => {
+		res.send('Hello world.');
+	});
 	app.use('*', (req, res) => {
 		res.status(404).json({ error: 'Not Found' });
 	});
